749ce0ecd2f943887e21bc0055f898238bd1df7e
Thobif <jirayootpandee537@gmail.com>
2021-09-13 17:21:10 +0700

    update new

---
+++ js/app104.js
@@ -0,0 +1,26 @@
+let firstName = 'Michel';
+let lastName = 'Jackson';
+var fullName = firstName + " " + lastName;
+
+console.log("10"+12);
+
+const a = "10"
+const b = 10;
+
+
+if(a === 10){
+    console.log("ok")
+}
+else{
+    console.log("No ok");
+}
+
+console.log("5" * 5);
+
+
+
+
+// console.log(firstName + " " + lastName);
+// console.log(fullName);
+
+// document.write(`<h2 id="text">${fullName}</h2>`);
